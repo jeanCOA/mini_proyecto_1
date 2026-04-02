7c0448f5f57ec2c0458b126374fe6a8453c540c4
JP-TechnologyDeveloper <joseph.puerta@correounivalle.edu.co>
2026-04-02 01:22:08 -0500

    Clase CartaMagica updated

--- src/CartaMagica.java
+++ src/CartaMagica.java
@@ -1,22 +1,19 @@
 public abstract class CartaMagica extends Carta implements Activable {
     protected String descripcion;
 
-    public CartaMagica (String nombre, String descripcion){
-        super(nombre); //llama al nombre de la clase carta que crea jean
+    public CartaMagica(String nombre, String descripcion) {
+        super(nombre, 0); // Las magias suelen ser nivel 0 según el nuevo UML
         this.descripcion = descripcion;
-
-
     }
 
+    @Override
+    public String getTipo() {
+        return "MAGICA";
+    }
+
+    @Override
     public String getDescripcion() {
         return descripcion;
     }
-
-    public abstract void aplicarEfecto(Jugador jugador);
-
-
-
-
-
     
 }

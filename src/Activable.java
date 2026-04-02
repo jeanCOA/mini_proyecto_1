d2ff8236a79174983bbd5d77c56f3f1abed0cad3
JP-TechnologyDeveloper <joseph.puerta@correounivalle.edu.co>
2026-04-02 18:47:19 -0500

    Dejo solo los archivos con de cada carta magica

--- src/Activable.java
+++
@@ -1,6 +0,0 @@
-public interface Activable {
-    
-    void Activar (contexto contexto);// Método que ejecuta la lógica de la carta
-
-    String getDescripcion();// Método para obtener el texto de lo que hace la carta
-}

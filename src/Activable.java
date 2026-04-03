8636f0c4909b95e495d67027e2fc136431d03d83
JP-TechnologyDeveloper <joseph.puerta@correounivalle.edu.co>
2026-04-02 19:05:39 -0500

    metodo actualizado segun el UML v4

--- src/Activable.java
+++ src/Activable.java
@@ -1,6 +1,3 @@
 public interface Activable {
-    
-    void Activar (contexto contexto);// Método que ejecuta la lógica de la carta
-
-    String getDescripcion();// Método para obtener el texto de lo que hace la carta
+    void activar(Contexto ctx);
 }

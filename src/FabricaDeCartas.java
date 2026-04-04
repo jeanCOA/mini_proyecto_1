6b5b1631442e1d7e36a0f07f32e834f3982cbd8b
JuanPabloRada <juan.pablo.rada@correounivalle.edu.co>
2026-04-03 19:22:56 -0500

    Agregue todos los monstruos y el metodo crearMazoCompleto

--- src/FabricaDeCartas.java
+++ src/FabricaDeCartas.java
@@ -1,7 +1,72 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class FabricaDeCartas {
+
+    public static List<Carta> crearMazoCompleto() {
+        List<Carta> mazo = new ArrayList<>();
+        mazo.addAll(crearMonstruos());
+        mazo.addAll(crearMagicas());
+        return mazo;
+    }
+
+
+    public static List<CartaMonstruo> crearMonstruos(){
+        List<CartaMonstruo> monstruos = new ArrayList<>();
+        
+        //Guerrero de la Luz 5 copias
+        monstruos.add(new CartaMonstruo("Guerrero De La Luz", 3, 1200, 1000));
+        monstruos.add(new CartaMonstruo("Guerrero De La Luz", 3, 1200, 1000));
+        monstruos.add(new CartaMonstruo("Guerrero De La Luz", 3, 1200, 1000));
+        monstruos.add(new CartaMonstruo("Guerrero De La Luz", 3, 1200, 1000));
+        monstruos.add(new CartaMonstruo("Guerrero De La Luz", 3, 1200, 1000));
+
+        //Bestia del bosque 5 copias
+        monstruos.add(new CartaMonstruo("Bestia del bosque", 4, 1500, 1200));
+        monstruos.add(new CartaMonstruo("Bestia del bosque", 4, 1500, 1200));
+        monstruos.add(new CartaMonstruo("Bestia del bosque", 4, 1500, 1200));
+        monstruos.add(new CartaMonstruo("Bestia del bosque", 4, 1500, 1200));
+        monstruos.add(new CartaMonstruo("Bestia del bosque", 4, 1500, 1200));
+
+
+        //Guardian del hierro 5 copias
+        monstruos.add(new CartaMonstruo("Guardian del hierro", 5, 1000, 2000));
+        monstruos.add(new CartaMonstruo("Guardian del hierro", 5, 1000, 2000));
+        monstruos.add(new CartaMonstruo("Guardian del hierro", 5, 1000, 2000));
+        monstruos.add(new CartaMonstruo("Guardian del hierro", 5, 1000, 2000));
+        monstruos.add(new CartaMonstruo("Guardian del hierro", 5, 1000, 2000));
+
+
+        //Hechicero del caos 5 copias
+        monstruos.add(new CartaMonstruo("Hechicero del caos", 4, 1800, 1500));
+        monstruos.add(new CartaMonstruo("Hechicero del caos", 4, 1800, 1500));
+        monstruos.add(new CartaMonstruo("Hechicero del caos", 4, 1800, 1500));
+        monstruos.add(new CartaMonstruo("Hechicero del caos", 4, 1800, 1500));
+        monstruos.add(new CartaMonstruo("Hechicero del caos", 4, 1800, 1500));
+
+
+        //Caballero real 6 copias
+        monstruos.add(new CartaMonstruo("Caballero Real", 6, 2300, 2000));
+        monstruos.add(new CartaMonstruo("Caballero Real", 6, 2300, 2000));
+        monstruos.add(new CartaMonstruo("Caballero Real", 6, 2300, 2000));
+        monstruos.add(new CartaMonstruo("Caballero Real", 6, 2300, 2000));
+        monstruos.add(new CartaMonstruo("Caballero Real", 6, 2300, 2000));
+        monstruos.add(new CartaMonstruo("Caballero Real", 6, 2300, 2000));
+
+
+        //Dragon Ancestral 4 copias
+        monstruos.add(new CartaMonstruo("Dragon Ancestral", 8, 3000, 2500));
+        monstruos.add(new CartaMonstruo("Dragon Ancestral", 8, 3000, 2500));
+        monstruos.add(new CartaMonstruo("Dragon Ancestral", 8, 3000, 2500));
+        monstruos.add(new CartaMonstruo("Dragon Ancestral", 8, 3000, 2500));
+
+        return monstruos;
+    }
+
+
+
+
 
     public static List<CartaMagica> crearMagicas() {
         List<CartaMagica> magicas = new ArrayList<>();
